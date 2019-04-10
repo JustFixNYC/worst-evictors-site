e5bd923c9fe145d326c49c18b39232edadc0307b
Sam Raby <Sam@Sams-MacBook-Pro-4.local>
2019-04-09 22:29:22 -0400

    added scaffolding and content for landing page

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -22,14 +22,7 @@ const Layout = ({ children }: Props) => (
           <html lang="en" />
         </Helmet>
         <Header />
-        <div
-          style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0,
-          }}
-        >
+        <div>
           {children}
         </div>
       </>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import { StaticQuery, graphql } from 'gatsby'
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 import { Link } from 'gatsby'
 
-import "spectre.css"
+import '../styles/index.scss'
 
 import Layout from '../components/layout'
 
@@ -15,53 +15,105 @@ const LandingPage = () => (
       query {
         contentfulLandingPage {
           openingTitle {
-                json
-              }
-              openingSubtitle {
-                json
-              }
-              openingBackground {
-                sizes(maxWidth: 613) {
-                            aspectRatio
-                            src
-                            srcSet
-                            sizes
-                          } 
-              }
-              listTitle
-              listDescription {
-                json
-              }
-              listButton
-              mapTitle
-              mapDescription {
-                json
-              }
-              mapButton
-              contextTitle
-              contextDescription {
-                json
-              }
-              efnycTitle
-              efnycDescription {
-                json
-              }
-              efnycButton
-              kyrTitle
-              kyrDescription {
-                json
-              }
-              kyrButton
+            json
+          }
+          openingSubtitle {
+            json
+          }
+          openingBackground {
+            sizes(maxWidth: 613) {
+                        aspectRatio
+                        src
+                        srcSet
+                        sizes
+                      } 
+          }
+          listTitle
+          listDescription {
+            json
+          }
+          listButton
+          mapTitle
+          mapDescription {
+            json
+          }
+          mapButton
+          contextTitle
+          contextDescription {
+            json
+          }
+          efnycTitle
+          efnycDescription {
+            json
+          }
+          efnycButton
+          kyrTitle
+          kyrDescription {
+            json
+          }
+          kyrButton
         }
       }
     `}
     render={data => (
       <Layout>
-        <h1>Hello!</h1>
-        <p>Welcome to your new Gatsby site!</p>
-        <p>Now go build something great.</p>
-        <p>{documentToReactComponents(data.contentfulLandingPage.openingTitle.json)}</p>
-        <Link to="/evictors-list/" className="btn primary">Go to page 2</Link>
+        <section id="intro" className="hero hero-lg bg-primary">
+          <div className="hero-body">
+            <h1>{documentToReactComponents(data.contentfulLandingPage.openingTitle.json)}</h1>
+            {documentToReactComponents(data.contentfulLandingPage.openingSubtitle.json)}
+          </div>
+        </section>
+        <section id="list-link" className="hero half-vh">
+          <div className="hero-body">
+            <h1>{data.contentfulLandingPage.listTitle}</h1>
+            {documentToReactComponents(data.contentfulLandingPage.listDescription.json)}
+            <Link to="/evictors-list" className="btn btn-primary">
+              {data.contentfulLandingPage.listButton}
+              <i className="icon icon-forward ml-2"></i>
+            </Link>
+          </div>
+        </section>
+        <section id="map-link" className="hero half-vh">
+          <div className="hero-body">
+            <h1>{data.contentfulLandingPage.mapTitle}</h1>
+            {documentToReactComponents(data.contentfulLandingPage.mapDescription.json)}
+            <Link to="/map" className="btn btn-primary">
+              {data.contentfulLandingPage.mapButton}
+              <i className="icon icon-forward ml-2"></i>
+            </Link>
+          </div>
+        </section>
+        <section id="context" className="hero">
+          <div className="hero-body">
+            <h3>{data.contentfulLandingPage.contextTitle}</h3>
+            <div className="divider d-invisible"></div>
+            {documentToReactComponents(data.contentfulLandingPage.contextDescription.json)}
+          </div>
+        </section>
+        <section id="resources" className="container">
+          <div className="columns">
+            <div id="efnyc" className="column col-6 col-sm-12 hero hero-sm">
+              <div className="hero-body">
+                <h3>{data.contentfulLandingPage.efnycTitle}</h3>
+                {documentToReactComponents(data.contentfulLandingPage.efnycDescription.json)}
+                <a href="https://www.evictionfreenyc.org/" className="btn btn-primary">
+                  {data.contentfulLandingPage.efnycButton}
+                  <i className="icon icon-forward ml-2"></i>
+                </a>
+              </div>
+            </div>
+            <div id="know-your-rights" className="column col-6 col-sm-12 hero hero-sm">
+              <div className="hero-body">
+                <h3>{data.contentfulLandingPage.kyrTitle}</h3>
+                {documentToReactComponents(data.contentfulLandingPage.kyrDescription.json)}
+                <Link to="/rights" className="btn btn-primary">
+                  {data.contentfulLandingPage.kyrButton}
+                  <i className="icon icon-forward ml-2"></i>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </section>
       </Layout>
     )}
   />
