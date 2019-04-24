ea7f382b1bac33def23a52bd9aaaf24fef8662e0
Sam Raby <Sam@Sams-MBP-4.brl.nyc>
2019-04-24 18:17:06 -0400

    fixed active button on footer bug

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -12,7 +12,7 @@ const Footer = () => (
         <span className="show-md">By <a href="https://www.righttocounselnyc.org/" target="_blank">RTC Coalition</a>, <a href="https://www.antievictionmap.com/" target="_blank">AEMP</a>, and <a href="https://www.justfix.nyc/" target="_blank">JustFix.nyc</a></span>
     </section>
     <section className="navbar-section">
-      <Link activeClassName="active" className="btn btn-link" to="/about">About</Link>
+      <Link className="btn btn-link" to="/about">About</Link>
       <a href="https://github.com/JustFixNYC/worst-evictors-site" className="btn btn-link"> <FaGithub className="icon" /></a>
     </section>
   </header>
