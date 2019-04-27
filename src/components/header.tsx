7d679d06a353d0eab6bc8df6173619e0b116925b
Sam Raby <Sam@Sams-MacBook-Pro-4.local>
2019-04-27 15:39:10 -0400

    header component refactoring

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,40 +7,61 @@ type Props = {
   siteTitle: string
 }
 
-const Header = ({ siteTitle }: Props) => (
-  <header className="navbar header p-fixed">
-    <section className="navbar-section"></section>
-    <section className="navbar-center"></section>
-    <section className="navbar-section hide-sm">
-      <Link activeClassName="active" className="btn btn-link" to="/">Home</Link>
-      <Link activeClassName="active" className="btn btn-link" to="/evictors-list">Worst Evictors List</Link>
-      <Link activeClassName="active" className="btn btn-link" to="/map">Evictions Map</Link>
-      <Link activeClassName="active" className="btn btn-link" to="/rights">My Rights</Link>
-      <Link activeClassName="active" className="btn btn-link" to="/about">About</Link>
-    </section>
-    <div className="dropdown dropdown-right show-sm">
-      <a href="#" className="btn btn-link dropdown-toggle m-2" tabIndex="0">
-        <i className="icon icon-menu"></i>
-      </a>
-      <ul className="menu menu-reverse">
-        <li className="menu-item">
+class Header extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      isDropdownVisible: false
+    }
+    
+    this.toggleDropdown = this.toggleDropdown.bind(this);
+  }
+  
+  toggleDropdown() {
+    this.setState({
+      isDropdownVisible: !this.state.isDropdownVisible
+    })
+  }
+  
+  render() {
+    return (
+      <header className="navbar header p-fixed">
+        <section className="navbar-section"></section>
+        <section className="navbar-center"></section>
+        <section className="navbar-section hide-sm">
           <Link activeClassName="active" className="btn btn-link" to="/">Home</Link>
-        </li>
-        <li className="menu-item">
           <Link activeClassName="active" className="btn btn-link" to="/evictors-list">Worst Evictors List</Link>
-        </li>
-        <li className="menu-item">
           <Link activeClassName="active" className="btn btn-link" to="/map">Evictions Map</Link>
-        </li>
-        <li className="menu-item">
           <Link activeClassName="active" className="btn btn-link" to="/rights">My Rights</Link>
-        </li>
-        <li className="menu-item">  
           <Link activeClassName="active" className="btn btn-link" to="/about">About</Link>
-        </li>
-      </ul>
-    </div>
-  </header>
-)
+        </section>
+        <div className="dropdown dropdown-right show-sm">
+          <a href="#" className={"btn btn-link dropdown-toggle m-2" + (this.state.isDropdownVisible ? " active" : "")}
+          onClick={this.toggleDropdown} tabIndex="0">
+            <i className={"icon " + (this.state.isDropdownVisible ? "icon-cross" : "icon-menu")}></i>
+          </a>
+          <ul className={"menu menu-reverse " + (this.state.isDropdownVisible ? "d-block" : "d-none")}>
+            <li className="menu-item">
+              <Link activeClassName="active" className="btn btn-link" to="/">Home</Link>
+            </li>
+            <li className="menu-item">
+              <Link activeClassName="active" className="btn btn-link" to="/evictors-list">Worst Evictors List</Link>
+            </li>
+            <li className="menu-item">
+              <Link activeClassName="active" className="btn btn-link" to="/map">Evictions Map</Link>
+            </li>
+            <li className="menu-item">
+              <Link activeClassName="active" className="btn btn-link" to="/rights">My Rights</Link>
+            </li>
+            <li className="menu-item">  
+              <Link activeClassName="active" className="btn btn-link" to="/about">About</Link>
+            </li>
+          </ul>
+        </div>
+      </header>
+    );
+  }
+}
+
 
 export default Header
