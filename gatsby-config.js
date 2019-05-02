8d314ab1fe1d1a9cfa1c894be9dd3a2f20e1a969
Sam Raby <Sam@Sams-MacBook-Pro-4.local>
2019-05-02 09:37:31 -0400

    added google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,5 +37,12 @@ module.exports = {
     {
       resolve: `gatsby-plugin-emotion`,
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // replace "UA-XXXXXXXXX-X" with your own Tracking ID
+        trackingId: "UA-67069242-7",
+      },
+    }
   ],
 }
