42f060c9efbb0a643836f124ad9a2f609ceb9834
Sam Raby <Sam@Sams-MacBook-Pro-4.local>
2019-05-03 15:21:31 -0400

    updated metatags and added social image

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -2,43 +2,38 @@ import React from 'react'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
 
-import '../styles/header-footer.scss';
+import '../styles/header-footer.scss'
+
+import socialImage from '../images/social-image.png'
 
 import Header from './header'
 import Footer from './footer'
 
-          {/* General tags 
-            <meta name="image" content={image} />
-            <link rel="canonical" href={url} /> */}
-
-            {/* OpenGraph tags 
-            <meta property="og:url" content={url} />
-            {isBlogPost ? <meta property="og:type" content="article" /> : null}
-            <meta property="og:title" content={title} />
-            <meta property="og:description" content={description} />
-            <meta property="og:image" content={image} />
-            <meta property="fb:app_id" content={seo.social.fbAppID} /> */}
-
-            {/* Twitter Card tags 
-            <meta name="twitter:card" content="summary_large_image" />
-            <meta name="twitter:creator" content={seo.social.twitter} />
-            <meta name="twitter:title" content={title} />
-            <meta name="twitter:description" content={description} />
-            <meta name="twitter:image" content={image} /> */}
-
 const Layout = ({ children }: Props) => (
       <>
-        <Helmet
-          title="NYC's Worst Evictors"
-          meta={[
-            { name: 'description', content: 'This website brings transparency to evictions, the landlords who benefit, and what you can do to fight back. We are here to fight with you. When you fight, we all win.' },
-            { name: 'keywords', content: 'nyc, worst, evictions, evictors, rtc, right, to, counsel, tenant, organizing, landlord, map' },
-            { property: 'og:url', content: 'www.worstevictorsnyc.org' },
-            { property: 'og:title', content: 'Is your landlord on our worst evictors list and evictions map? Learn your rights here!' },
-            { property: 'og:description', content: 'This website brings transparency to evictions, the landlords who benefit, and what you can do to fight back. We are here to fight with you. When you fight, we all win.' },
-          ]}
-          >
+        <Helmet>
           <html lang="en" />
+          <title>NYC's Worst Evictors</title>
+          <meta name="description" content="This website brings transparency to evictions, the landlords who benefit, and what you can do to fight back. We are here to fight with you. When you fight, we all win." />
+          <meta name="keywords" content="nyc, worst, evictions, evictors, rtc, right, to, counsel, tenant, organizing, landlord, map" />
+          <meta name="author" content="Right to Counsel Coalition" />
+
+          <meta property="fb:app_id" content="247990609143668" />
+          <meta property="og:site_name" content="NYC's Worst Evictors" />
+          <meta property="og:title" content="Is your landlord on our list and map of NYC's worst evictors? Learn your rights here!" />
+          <meta property="og:description" content="This website brings transparency to evictions, the landlords who benefit, and what you can do to fight back. We are here to fight with you. When you fight, we all win." />
+          <meta property="og:url" content="https://www.worstevictorsnyc.org" />
+          <meta property="og:image" content={socialImage} />
+          <meta property="og:type" content="website" />
+
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:site" content="@RTCNYC" />
+          <meta name="twitter:creator" content="@RTCNYC" />
+          <meta name="twitter:title" content="Is your landlord on our list and map of NYC's worst evictors? Learn your rights here!" />
+          <meta name="twitter:description" content="This website brings transparency to evictions, the landlords who benefit, and what you can do to fight back. We are here to fight with you. When you fight, we all win." />
+          <meta name="twitter:url" content="https://www.worstevictorsnyc.org" />
+          <meta name="twitter:image" content={socialImage} />
+          <meta name="twitter:image:alt" content="NYC's Worst Evictors" />
         </Helmet>
         <Header />
         <div className="page-content">
