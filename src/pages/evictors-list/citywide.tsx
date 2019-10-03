8b8dd94ba4edacfe64a366054d559bbf5c866f5c
Sam Raby <Sam@Sams-MacBook-Pro-4.local>
2019-10-03 11:14:56 -0400

    refactored evictor component to account for citywide data

--- src/pages/evictors-list/citywide.tsx
+++ src/pages/evictors-list/citywide.tsx
@@ -16,99 +16,45 @@ import laurel4 from '../../images/laurel4.png'
 
 import Layout from '../../components/layout'
 import BoroList from '../../components/boro-list'
+import Evictor from '../../components/evictor'
 
 const EvictorsListPage = () => (
   <StaticQuery
     query={graphql`
       query {
-        contentfulListPage {
+        contentfulCitywideListPage {
           title
           subtitle {
             json
           }
-          evictorList {
-            boroName
-            subtitle {
-              json
+          evictors {
+            boro
+            citywideRank
+            name
+            corporation
+            photo {
+              sizes(maxWidth: 613) {
+                aspectRatio
+                src
+                srcSet
+                sizes
+              }
             }
-            evictors {
-              boro
-              rank
-              name
-              corporation
-              description {
-                json
-              }
-              photo {
-                sizes(maxWidth: 613) {
-                  aspectRatio
-                  src
-                  srcSet
-                  sizes
-                }
-              }
-              photoCaption
-              evictions
-              boroUnits
-              boroFilings
-              boroPercentFiled
-              boroPercentRs
-              citywideEvictions
-              citywideUnits
-              citywideFilings
-              citywidePercentFiled
-              citywidePercentRs
-              banks
-              lawyers
-              organizingCta {
-                json
-              }
-              organizingPhoto1 {
-                fluid {
-                  aspectRatio
-                  src
-                  srcSet
-                  sizes
-                }
-              }
-              organizingPhotoCaption1
-              organizingPhoto2 {
-                fluid {
-                  aspectRatio
-                  src
-                  srcSet
-                  sizes
-                }
-              }
-              organizingPhotoCaption2
-              organizingPhoto3 {
-                fluid {
-                  aspectRatio
-                  src
-                  srcSet
-                  sizes
-                }
-              }
-              organizingPhotoCaption3
-              evictionsMapUrl {
-                evictionsMapUrl
-              }
-              whoOwnsWhatUrl
+            photoCaption
+            citywideEvictions
+            citywideUnits
+            citywideFilings
+            citywidePercentFiled
+            citywidePercentRs
+            banks
+            lawyers
+            evictionsMapUrl {
+              evictionsMapUrl
             }
+            whoOwnsWhatUrl
           }
-          dishonorableMentions {
-            landlordsOnMultipleLists {
-              json
-            }
-            landlordsWhoSueEveryone {
-              json
-            }
-            landlordsWhoSueAlmostEveryone {
-              json
-            }
-            notableMentions {
-              json
-            }
+          citywideDishonorableMentions {
+            json
           }
         }
       }
@@ -117,19 +63,19 @@ const EvictorsListPage = () => (
       <Layout className="list-page">
         <section id="list-intro" className="list-hero hero bg-secondary text-light">
           <div className="hero-body">
-            <h1>{data.contentfulListPage.title}</h1>
+            <h1>{data.contentfulCitywideListPage.title}</h1>
             <span className="list-subtitle">
-              {documentToReactComponents(data.contentfulListPage.subtitle.json, contentfulOptions)}
+              {documentToReactComponents(data.contentfulCitywideListPage.subtitle.json, contentfulOptions)}
             </span>
             <h3> Select a borough: </h3>
           </div>
         </section>
         <div className="list-content">
-          <div className="boro-list-selector hero bg-secondary text-light">
+          {/* <div className="boro-list-selector hero bg-secondary text-light">
             <div className="hero-body">
               <div>
                 <div className="btn-group show-md">
-                  {data.contentfulListPage.evictorList.map(list => 
+                  {data.contentfulCitywideListPage.evictorList.map(list => 
                     <AnchorLink 
                       offset='50'
                       key={list.boroName} 
@@ -140,7 +86,7 @@ const EvictorsListPage = () => (
                   )}
                 </div>
                 <div className="btn-group hide-md">
-                  {data.contentfulListPage.evictorList.map(list => 
+                  {data.contentfulCitywideListPage.evictorList.map(list => 
                     <AnchorLink 
                       offset='120'
                       key={list.boroName} 
@@ -152,15 +98,21 @@ const EvictorsListPage = () => (
                 </div>
               </div>
             </div>
-          </div>
+          </div> */}
 
           <div className="red-space bg-secondary" />
 
-          {data.contentfulListPage.evictorList.map(list => 
-          <BoroList 
-            key={list.boroName} 
-            data={list} />
-          )}
+
+
+          <section className="boro-list">
+            {data.contentfulCitywideListPage.evictors.map(evictor => 
+              <Evictor 
+              key={evictor.name + '-' + evictor.boro} 
+              data={evictor} 
+              isCitywideEvictor={true}/>
+            )}
+          </section>
+          
           
         </div>
         <section id="dishonorable-mentions" className="list-hero hero bg-secondary text-light">
@@ -169,28 +121,9 @@ const EvictorsListPage = () => (
             <div className="mention-category">
               <div className="laurel container p-relative">
                 <img className="laurel-img p-centered" src={laurel1} alt="laurel1"/>
-                <div className="laurel-text text-center p-absolute">ON MULTIPLE BOROUGH LISTS</div>
               </div>
               <span className="text-center">
-                {documentToReactComponents(data.contentfulListPage.dishonorableMentions.landlordsOnMultipleLists.json)}
-              </span>
-            </div>
-            <div className="mention-category">
-              <div className="laurel container p-relative">
-                <img className="laurel-img p-centered" src={laurel2} alt="laurel2"/>
-                <div className="laurel-text text-center p-absolute">SUES PEOPLE MORE THAN ONCE</div>
-              </div>
-               <span className="text-center">
-                {documentToReactComponents(data.contentfulListPage.dishonorableMentions.landlordsWhoSueEveryone.json)}
-              </span>
-            </div>
-            <div className="mention-category">
-              <div className="laurel container p-relative">
-                <img className="laurel-img p-centered" src={laurel4} alt="laurel4"/>
-                <div className="laurel-text text-center p-absolute">SUES A LOT</div>
-              </div>
-               <span className="text-center">
-                {documentToReactComponents(data.contentfulListPage.dishonorableMentions.landlordsWhoSueAlmostEveryone.json)}
+                {documentToReactComponents(data.contentfulCitywideListPage.citywideDishonorableMentions.json)}
               </span>
             </div>
           </div>
