0600a66fc14e208373e755a4c55c3daf89c8ebb2
Sam Rabiyah <sraby@users.noreply.github.com>
2021-12-10 17:01:13 -0500

    Make all links go outbound

--- src/utils/contentful-rich-text-options.js
+++ src/utils/contentful-rich-text-options.js
@@ -1,9 +1,16 @@
 import React from "react";
-import { MARKS } from "@contentful/rich-text-types";
+import { MARKS, INLINES } from "@contentful/rich-text-types";
 
 const contentfulOptions = {
   renderMark: {
     [MARKS.UNDERLINE]: text => <span>{text}</span>
+  },
+  renderNode: {
+    [INLINES.HYPERLINK]: ({ data }, children) => (
+      <a href={data.uri} target="_blank" rel="noopener noreferrer">
+        {children}
+      </a>
+    )
   }
 };
 
