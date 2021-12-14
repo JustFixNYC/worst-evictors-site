63e4be330c27b2c19d5fd764e8f615664d90be96
Sam Rabiyah <sraby@users.noreply.github.com>
2021-12-14 14:04:02 -0500

    Add updated social share image

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -38,7 +38,7 @@ const Layout = ({
     customDescription ||
     "Never-before-seen data from NYC Housing Court highlights who stands to benefit the most if and when evictions move forward during COVID-19.";
   const url = customUrl || "https://www.worstevictorsnyc.org";
-  const shareImageURL = customImage || "https://i.imgur.com/RkMxfbS.png";
+  const shareImageURL = customImage || "https://i.imgur.com/FjrtF1X.png";
 
   return (
     <div className={className}>
